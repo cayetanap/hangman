61349ff7764ec733ca751641e5117c131ab5ca8d
Cayetana Pablos <cayetanap@gmail.com>
2017-05-20 16:11:39 +0200

    model finish

---
+++ application.js
@@ -0,0 +1,89 @@
+var hangman;
+
+var newGameClick = function() {
+  _initializeControls();
+  hangman = new Hangman();
+  drawCurrentWord();
+  document.getElementById("game").classList.remove("hide");
+};
+
+var drawCurrentWord = function(word) {
+  resetCurrentWord();
+
+  var currentWord = word || hangman.getWordStatus();
+  var currentWordDom = document.getElementById("currentWord");
+
+  currentWord.forEach(function(letter) {
+    var spanLetter = document.createElement("span");
+    var content = document.createTextNode(letter);
+
+    spanLetter.appendChild(content);
+    currentWordDom.appendChild(spanLetter);
+  });
+};
+
+var resetCurrentWord = function() {
+  var word = document.getElementById("currentWord");
+
+  while (word.firstChild) {
+    word.removeChild(word.firstChild);
+  }
+};
+
+var insertLetter = function(event) {
+  if (!hangman || (event.keyCode < 65 || event.keyCode > 90))
+    return;
+
+  var letter = String.fromCharCode(event.keyCode);
+  var correct = hangman.askLetter(letter);
+
+  if (correct !== undefined && !correct) {
+    _addLetter(letter);
+  } else {
+    drawCurrentWord();
+  }
+  drawHangman();
+  afterRound();
+
+};
+
+var _addLetter = function(letter) {
+  var currentLetter = letter;
+  var currentLetterDom = document.getElementById("letters");
+
+  var spanLetter = document.createElement("span");
+  var content = document.createTextNode(letter);
+
+  spanLetter.appendChild(content);
+  currentLetterDom.appendChild(spanLetter);
+};
+
+var drawHangman = function() {
+  document.getElementById("hangman").classList += " lives-" + (hangman.errorsLeft + 1);
+};
+
+var afterRound = function() {
+  var status = hangman.gameStatus();
+
+  if (!status)
+    return;
+
+  if (status.toLowerCase() === "you win") {
+    document.getElementById("you-win").classList = "";
+  } else {
+    drawCurrentWord(hangman.secretWord.split(""));
+    document.getElementById("game-over").classList = "";
+  }
+
+  hangman = undefined;
+};
+
+var _initializeControls = function() {
+  document.getElementById("you-win").classList = "hide";
+  document.getElementById("game-over").classList = "hide";
+  document.getElementById("hangman").classList = "";
+  document.getElementById("letters").innerHTML = "";
+};
+
+document.getElementById("new-game").addEventListener("click", newGameClick);
+document.addEventListener("keydown", insertLetter);
